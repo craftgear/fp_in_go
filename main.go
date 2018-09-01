db6c6f90c25f9f4aeb66087a261a3c3f7901fe36
Shunsuke Watanabe <craftgear@gmail.com>
2018-09-01 17:12:02 +0900

    add sum

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 
 	"craftgear.net/fp_in_go/fibonacci"
+	"craftgear.net/fp_in_go/sum"
 )
 
 func main() {
 	fib50 := fibonacci.MyFibMemoized(50)
 	fmt.Printf("fib50 = %+v\n", fib50)
+	sum10 := sum.Sum([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	fmt.Printf("sum10 = %+v\n", sum10)
+	sum10WithAccum := sum.SumWithAccumulator([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 0)
+	fmt.Printf("sum10 = %+v\n", sum10WithAccum)
 }
